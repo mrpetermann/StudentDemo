9bf85d7497f63368f88cc76e7c5aaf85a3f400ca
mpetermann <mpetermann@waukesha.k12.wi.us>
2022-10-03 13:46:48 -0500

    refactor with student name

--- src/Student.java
+++ src/Student.java
@@ -1,16 +1,40 @@
 public class Student {
+    private String firstName;
+    private String lastName;
     private double lunchBalance;
-    private String studentId;
+    private int studentId;
 
-    public Student(double lunchBalance, String studentId) {
-        this.lunchBalance = lunchBalance;
-        this.studentId = studentId;
+    //accept name, random id
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.lunchBalance = 0; //Default lunch balance
+        this.studentId = (int)Math.floor(Math.random() * 100000);
     }
 
+    //accept name, lunch bal, random id
+    public Student(String firstName, String lastName, double lunchBalance) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.lunchBalance = lunchBalance;
+        this.studentId = (int)Math.floor(Math.random() * 100000);
+    }
+
+    //name, get full name
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public String getFullName() {
+        return this.firstName.concat(" ").concat(this.lastName);
+    }
+
+    //lunch balance
     public double getLunchBalance(){ return this.lunchBalance; }
     public void setLunchBalance(double lunchBalance) { this.lunchBalance = lunchBalance; }
 
-    public String getStudentId(){ return this.studentId; }
-    public void setStudentId(String studentId) { this.studentId = studentId; }
+    //student id
+    public int getStudentId(){ return this.studentId; }
+    public void setStudentId(int studentId) { this.studentId = studentId; }
 }
 
