7bd8596533efa9b53726310fdc2f8aac75335b18
mpetermann <mpetermann@waukesha.k12.wi.us>
2022-11-01 14:01:35 -0500

    get student data from user and validate lunch balance

--- src/StudentDemo.java
+++ src/StudentDemo.java
@@ -1,18 +1,35 @@
+import java.util.Scanner;
+
 public class StudentDemo {
     public static void main(String[] a) {
-        //Instantiate students A and B
-        Student studentA = new Student("Brad", "Johnson");
-        Student studentB = new Student("Ray", "Schmitt", 125);
+        Scanner in = new Scanner(System.in);
 
-        //Output student A full name + id
-        System.out.println("Student A: " + studentA.getFullName());
-        System.out.println("ID: " + studentA.getStudentId());
-        System.out.println("Balance: $" + studentA.getLunchBalance());
+        String firstName;
+        String lastName;
+        double lunchBalance;
 
-        //Output student B full name + id
-        System.out.println("Student B: " + studentB.getFullName());
-        System.out.println("ID: " + studentB.getStudentId());
-        System.out.println("Balance: $" + studentB.getLunchBalance());
+        System.out.println("Enter student first name:");
+        firstName = in.nextLine();
+        System.out.println("Enter student last name:");
+        lastName = in.nextLine();
+        System.out.println("Enter initial lunch balance:");
+        lunchBalance = in.nextDouble();
+
+        if(lunchBalance >= 0) {
+            //Instantiate student
+            Student student = new Student(firstName, lastName, lunchBalance);
+
+            //Output student full name + id
+            System.out.println("Student: " + student.getFullName());
+            System.out.println("ID: " + student.getStudentId());
+            System.out.println("Balance: $" + student.getLunchBalance());
+        }
+        else {
+            System.out.println("Invalid initial deposit.");
+        }
+
+        //Closing connection to terminal
+        in.close();
     }
 }
 
