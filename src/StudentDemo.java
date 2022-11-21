18626ab7e76c010e96e743934b5b32143c3e2301
mpetermann <mpetermann@waukesha.k12.wi.us>
2022-11-21 13:30:41 -0600

    Use while loop to produce good input for lunch balance

--- src/StudentDemo.java
+++ src/StudentDemo.java
@@ -7,55 +7,55 @@ public class StudentDemo {
 
         String firstName;
         String lastName;
-        double lunchBalance;
+        double lunchBalance = 0;
+        boolean isValid;
 
         //Get values from user for studentA
         System.out.println("Enter student first name:");
         firstName = in.nextLine();
         System.out.println("Enter student last name:");
         lastName = in.nextLine();
-        System.out.println("Enter initial lunch balance:");
-        lunchBalance = Double.parseDouble(in.nextLine());
 
-        Student studentA = null;
-        if(lunchBalance >= 0) {
-            //Instantiate student
-            studentA = new Student(firstName, lastName, lunchBalance);
+        isValid = false;
+        while(!isValid) {
+            System.out.println("Enter initial lunch balance:");
+            lunchBalance = Double.parseDouble(in.nextLine());
 
-            //Output student full name + id
-            System.out.println("Student: " + studentA.getFullName());
-            System.out.println("ID: " + studentA.getStudentId());
-            System.out.println("Balance: $" + studentA.getLunchBalance());
+            isValid = (lunchBalance >= 0);
         }
-        else {
-            System.out.println("Invalid initial deposit.");
-        }
+
+        //Create studentA
+        Student studentA = new Student(firstName, lastName, lunchBalance);
+
+        //Output student full name + id
+        System.out.println("Student: " + studentA.getFullName());
+        System.out.println("ID: " + studentA.getStudentId());
+        System.out.println("Balance: $" + studentA.getLunchBalance());
 
         //Get values from user for studentB
         System.out.println("Enter student first name:");
         firstName = in.nextLine();
         System.out.println("Enter student last name:");
         lastName = in.nextLine();
-        System.out.println("Enter initial lunch balance:");
-        lunchBalance = Double.parseDouble(in.nextLine());
 
-        Student studentB = null;
-        if(lunchBalance >= 0) {
-            //Instantiate student
-            studentB = new Student(firstName, lastName, lunchBalance);
+        isValid = false;
+        while(!isValid) {
+            System.out.println("Enter initial lunch balance:");
+            lunchBalance = Double.parseDouble(in.nextLine());
 
-            //Output student full name + id
-            System.out.println("Student: " + studentB.getFullName());
-            System.out.println("ID: " + studentB.getStudentId());
-            System.out.println("Balance: $" + studentB.getLunchBalance());
-        }
-        else {
-            System.out.println("Invalid initial deposit.");
+            isValid = (lunchBalance >= 0);
         }
 
-        //If students exist, test whether they are the same
-        if(studentA != null && studentB != null)
-            System.out.println("Students are same: " + studentA.equals(studentB));
+        //Create studentB
+        Student studentB = new Student(firstName, lastName, lunchBalance);
+
+        //Output student full name + id
+        System.out.println("Student: " + studentB.getFullName());
+        System.out.println("ID: " + studentB.getStudentId());
+        System.out.println("Balance: $" + studentB.getLunchBalance());
+
+        //Test whether two students are the same
+        System.out.println("Students are same: " + studentA.equals(studentB));
 
         //Close connection to terminal
         in.close();
