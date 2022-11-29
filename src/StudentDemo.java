c36cebdf67307d1872e3da2b02514152b3e7a9a5
mpetermann <mpetermann@waukesha.k12.wi.us>
2022-11-29 13:56:30 -0600

    Convert to do while and create get/print methods

--- src/StudentDemo.java
+++ src/StudentDemo.java
@@ -2,13 +2,27 @@ import java.util.Scanner;
 
 public class StudentDemo {
     public static void main(String[] a) {
-        //Creating a scanner to read user input from console
-        Scanner in = new Scanner(System.in);
+        //Create students
+        Student studentA = getStudent();
+        Student studentB = getStudent();
 
+        //Print students
+        printStudent(studentA);
+        printStudent(studentB);
+
+        //Test whether two students are the same
+        System.out.println("Students are same: " + studentA.equals(studentB));
+    }
+
+    /**
+     * Create a new student with console input
+     * @return Student from user input
+     */
+    private static Student getStudent() {
+        Scanner in = new Scanner(System.in);
         String firstName;
         String lastName;
         double lunchBalance = 0;
-        boolean isValid;
 
         //Get values from user for studentA
         System.out.println("Enter student first name:");
@@ -16,49 +30,25 @@ public class StudentDemo {
         System.out.println("Enter student last name:");
         lastName = in.nextLine();
 
-        isValid = false;
-        while(!isValid) {
+        //Get valid lunch balance
+        do {
             System.out.println("Enter initial lunch balance:");
             lunchBalance = Double.parseDouble(in.nextLine());
+        } while(lunchBalance < 0);
 
-            isValid = (lunchBalance >= 0);
-        }
-
-        //Create studentA
-        Student studentA = new Student(firstName, lastName, lunchBalance);
-
-        //Output student full name + id
-        System.out.println("Student: " + studentA.getFullName());
-        System.out.println("ID: " + studentA.getStudentId());
-        System.out.println("Balance: $" + studentA.getLunchBalance());
-
-        //Get values from user for studentB
-        System.out.println("Enter student first name:");
-        firstName = in.nextLine();
-        System.out.println("Enter student last name:");
-        lastName = in.nextLine();
-
-        isValid = false;
-        while(!isValid) {
-            System.out.println("Enter initial lunch balance:");
-            lunchBalance = Double.parseDouble(in.nextLine());
-
-            isValid = (lunchBalance >= 0);
-        }
-
-        //Create studentB
-        Student studentB = new Student(firstName, lastName, lunchBalance);
-
-        //Output student full name + id
-        System.out.println("Student: " + studentB.getFullName());
-        System.out.println("ID: " + studentB.getStudentId());
-        System.out.println("Balance: $" + studentB.getLunchBalance());
-
-        //Test whether two students are the same
-        System.out.println("Students are same: " + studentA.equals(studentB));
-
-        //Close connection to terminal
         in.close();
+
+        return new Student(firstName, lastName, lunchBalance);
+    }
+
+    /**
+     * Print student data
+     * @param student Student object that contains data to print
+     */
+    private static void printStudent(Student student) {
+        System.out.println("Student: " + student.getFullName());
+        System.out.println("ID: " + student.getStudentId());
+        System.out.println("Balance: $" + student.getLunchBalance());
     }
 }
 
